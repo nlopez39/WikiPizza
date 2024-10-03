5e36d0ff4f504d708b380cc57a6798c5dadcc402
Nelly Lopez <nlopez53@ucmerced.edu>
2024-10-03 11:03:23 -0700

    added file routes

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,3 +1,7 @@
+import { NavLinks } from '@/app/ui/nav-links'
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 export default function RootLayout({
   children,
 }: {
@@ -6,6 +10,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <NavLinks/>
         {/* Layout UI */}
         <main>{children}</main>
       </body>
---
+++ client/app/ui/nav-links.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { usePathname } from "next/navigation"
+import Link from 'next/link'
+
+export  function NavLinks(){
+ const pathname = usePathname(); 
+
+ return (
+    // <nav>
+    //   <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">
+    //     Home
+    //   </Link>
+ 
+    //   <Link
+    //     className={`link ${pathname === '/dashboard' ? 'active' : ''}`}
+    //     href="/dashboard"
+    //   >
+    //     Dashboard
+    //   </Link>
+    // </nav>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+  <Link className="navbar-brand" href="/">WikiPizza</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+     
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+        <Link className={`nav-link ${pathname === '/dashboard' ? 'active' : ''}`} href="/dashboard">
+      Dashboard
+      </Link>
+        </li>
+        <li className="nav-item">
+        <Link className={`nav-link ${pathname === '/dashboard' ? 'active' : ''}`} href="/posts">
+      Posts
+      </Link>
+        </li>
+    
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+ )
+}
