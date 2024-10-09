c261914bd4bf03e7a507002b3bae842f3d8a057b
Nelly Lopez <nlopez53@ucmerced.edu>
2024-10-09 11:58:22 -0700

    created a post form

--- client/app/blogpost/page.tsx
+++ client/app/blogpost/page.tsx
@@ -1,5 +1,22 @@
 //this is the code that will output on the page, you can also import the blogpost component here 
 //make a post
+import './style.css';
 export default function Page(){
-    return <h1>Start a Post</h1>
+
+    return <div className='form-container'>
+         <form className="form-horizontal"  role="form">
+    <div className="mb-3">
+      <label className="form-label">Title</label>
+      <input type="title" className="form-control" id="titleInput" aria-describedby="titleHelp"/>
+     
+    </div>
+    <div className="mb-3">
+      <label className="form-label">Content</label>
+      <textarea className="form-control" id="exampleFormControlTextarea1" rows={3}></textarea>
+    </div>
+    
+    <button type="submit" className="btn btn-primary">Post</button>
+  </form>
+    </div>
+   
 }
