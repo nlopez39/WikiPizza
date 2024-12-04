edcfd59f103f989be88b0aee223b3642e63304b4
Nelly Lopez <nlopez53@ucmerced.edu>
2024-12-03 16:11:44 -0800

    made localstorage client only

--- client/app/feed/page.tsx
+++ client/app/feed/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import './style.css';
-import React, { useState, useContext} from 'react';
+import React, { useState, useEffect} from 'react';
 
 import { jwtDecode } from "jwt-decode";
 import {GET_POST} from "../utils/queries";
@@ -13,17 +13,31 @@ export default function Page() {
   //extract the author id from the query and compare it with the user id in the token 
 //if the id's match then filter and map those posts under feed 
 //grab the id from the token 
-const token = localStorage.getItem('id_token');
+// const token = localStorage.getItem('id_token');
 //save the user _id  from the token to userId variable
-var userId =0; 
-//decode the token
-if(token){
-  const decodedToken:any = jwtDecode(token);
-  userId = decodedToken.data._id; 
-}
+const [userId, setUserId] = useState<number | null>(null);
+// Ensure the token logic runs only on the client
+useEffect(() => {
+  const token = localStorage.getItem('id_token');
+  if (token) {
+    const decodedToken: any = jwtDecode(token);
+    setUserId(decodedToken.data._id); // Save the user ID in state
+  }
+}, []);
 
 //get post 
 const { loading, data } = useQuery(GET_POST);
+
+if (loading) {
+  return <p>Loading...</p>;
+}
+
+if (userId === null) {
+  return <p>Please log in to view your feed.</p>;
+}
+
+
+
     return <> <div className='list-group'>
    
   
